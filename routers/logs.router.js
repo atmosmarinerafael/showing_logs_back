4f2d5d009c9456aa292d43693f0857e289f32fd2
atmosmarinerafael <rafael@atmosmarine.com>
2023-09-18 15:20:45 -0300

    feat: auth middleware

--- routers/logs.router.js
+++ routers/logs.router.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { addLog, findLogs } from "../controllers/logs.controller.js";
+import { authenticateToken } from "../middlewares/auth.middleware.js";
 
 
 const logsRouter = Router();
 
-logsRouter.get("/logs", findLogs);
-logsRouter.post("/logs", addLog);
+logsRouter.get("/logs", authenticateToken, findLogs);
+logsRouter.post("/logs", authenticateToken, addLog);
 
 export default logsRouter;
